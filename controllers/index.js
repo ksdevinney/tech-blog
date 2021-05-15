cf1cb7b655236ed701312fb80c1f13463ef3cb2e
ksdevinney <katie@devinney.org>
2021-05-14 20:52:14 -0500

    api posts route works but no others

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const blogRoutes = require('./blogRoutes');
 
+// router.use('/', adminRoutes);
 router.use('/api', apiRoutes);
 router.use('/api/posts', blogRoutes);
 
