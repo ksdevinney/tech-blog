0400774fca8d5de34d95b3136c31b18d6326fe38
ksdevinney <katie@devinney.org>
2021-05-14 20:24:14 -0500

    fix server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-// from mini-project starter code
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const session = require('express-session');
+const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -18,23 +17,22 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
-  })
+  }),
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-​
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
