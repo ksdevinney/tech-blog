eb6625e59d3b9e3c7d97bb88b3e9e221b9c20f23
ksdevinney <katie@devinney.org>
2021-11-27 20:40:06 -0600

    layouts and routes

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 app.use(require("./controllers/temp/index"))
 app.use(require("./controllers/temp/htmlRoutes"))
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
